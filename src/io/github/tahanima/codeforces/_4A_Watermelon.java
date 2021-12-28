a0964903e3da441f9f172eb8f2529ce2b366e3f1
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-12-28 23:33:17 +0600

    Add solution for CF 4A

---
+++ src/io/github/tahanima/codeforces/_4A_Watermelon.java
@@ -0,0 +1,16 @@
+package io.github.tahanima.codeforces;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 12/28/2021
+ */
+public class _4A_Watermelon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        byte w = scanner.nextByte();
+
+        System.out.printf("%s\n", w < 4 ? "NO" : ((w - 2) % 2 == 0 ? "YES" : "NO"));
+    }
+}
