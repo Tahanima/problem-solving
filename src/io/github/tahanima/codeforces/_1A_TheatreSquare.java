46886ab134c9372903b7f0dffdec889f9da172e1
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-12-31 13:50:48 +0600

    Add CF 1A solution

---
+++ src/io/github/tahanima/codeforces/_1A_TheatreSquare.java
@@ -0,0 +1,21 @@
+package io.github.tahanima.codeforces;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 12/31/2021
+ */
+public class _1A_TheatreSquare {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextInt();
+        long m = scanner.nextInt();
+        long a = scanner.nextInt();
+
+        long ans = (n / a) + (n % a == 0 ? 0 : 1);
+        ans *= ((m / a) + (m % a == 0 ? 0 : 1));
+
+        System.out.println(ans);
+    }
+}
