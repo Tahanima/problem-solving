480b5720d19945363d9c1a784a2963f1b126c671
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-12-31 13:51:35 +0600

    Add CF 71A solution

---
+++ src/io/github/tahanima/codeforces/_71A_WayTooLongWords.java
@@ -0,0 +1,21 @@
+package io.github.tahanima.codeforces;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 12/31/2021
+ */
+public class _71A_WayTooLongWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            String word = scanner.next();
+            int len = word.length();
+
+            System.out.printf("%s\n", len > 10 ? (word.charAt(0) + "" + (len - 2) + "" + word.charAt(len - 1)): word);
+        }
+    }
+}
