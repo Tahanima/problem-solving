7972f9bc70ccbc79f8df0f9416f0cce83db04766
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-07 02:13:36 +0600

    Add UVa 591 solution

---
+++ src/io/github/tahanima/uva/_591_BoxOfBricks.java
@@ -0,0 +1,52 @@
+package io.github.tahanima.uva;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 01/07/2022
+ */
+public class _591_BoxOfBricks {
+    final static int MAX = 55;
+    final static int[] height = new int[MAX];
+    final static Scanner scanner = new Scanner(System.in);
+
+    public static String solve(int n) {
+        int perStackHeight = 0;
+        int bricks = 0;
+
+        for (int i = 0; i < n; i++) {
+            height[i] = scanner.nextInt();
+            perStackHeight += height[i];
+        }
+
+        perStackHeight /= n;
+
+        for (int i = 0; i < n; i++) {
+            bricks += Math.abs(perStackHeight - height[i]);
+        }
+
+        return "The minimum number of moves is " + (bricks / 2) + ".";
+    }
+
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int testCase = 1;
+
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+
+            if (n == 0) {
+                break;
+            }
+
+            stringBuilder.append("Set #")
+                    .append(testCase++)
+                    .append("\n")
+                    .append(solve(n))
+                    .append("\n\n");
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
