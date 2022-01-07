cdab29f39ab97c09e7dccfb256e38395b05dfbf4
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-08 01:08:43 +0600

    Add UVa 11727 solution

---
+++ src/io/github/tahanima/uva/_11727_CostCutting.java
@@ -0,0 +1,32 @@
+package io.github.tahanima.uva;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 01/08/2022
+ */
+public class _11727_CostCutting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        int testCases = scanner.nextInt();
+        int[] salary = new int[3];
+
+        for (int i = 1; i <= testCases; i++) {
+            for (int j = 0; j < 3; j++) {
+                salary[j] = scanner.nextInt();
+            }
+
+            Arrays.sort(salary);
+            stringBuilder.append("Case ")
+                    .append(i)
+                    .append(": ")
+                    .append(salary[1])
+                    .append("\n");
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
