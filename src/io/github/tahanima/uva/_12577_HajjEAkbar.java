4d9f74ac15d1c5d822b78b9814a0b911427c6eb9
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-08 01:10:13 +0600

    Add UVa 12577 solution

---
+++ src/io/github/tahanima/uva/_12577_HajjEAkbar.java
@@ -0,0 +1,31 @@
+package io.github.tahanima.uva;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 01/08/2022
+ */
+public class _12577_HajjEAkbar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        int testCases = 1;
+
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+
+            if (word.equals("*")) {
+                break;
+            }
+
+            stringBuilder.append("Case ")
+                    .append(testCases++)
+                    .append(": ")
+                    .append(word.equals("Hajj") ? "Hajj-e-Akbar" : "Hajj-e-Asghar")
+                    .append("\n");
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
