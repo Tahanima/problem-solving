a514f07d8c0caca8a35f124372501fcbb83edd79
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-08 01:09:12 +0600

    Add UVa 12250 solution

---
+++ src/io/github/tahanima/uva/_12250_LanguageDetection.java
@@ -0,0 +1,40 @@
+package io.github.tahanima.uva;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 01/08/2022
+ */
+public class _12250_LanguageDetection {
+    public static void main(String[] args) {
+        HashMap<String, String> mapping = new HashMap<>();
+        mapping.put("HELLO", "ENGLISH");
+        mapping.put("HOLA", "SPANISH");
+        mapping.put("HALLO", "GERMAN");
+        mapping.put("BONJOUR", "FRENCH");
+        mapping.put("CIAO", "ITALIAN");
+        mapping.put("ZDRAVSTVUJTE", "RUSSIAN");
+
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        int testCases = 1;
+
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+
+            if (word.equals("#")) {
+                break;
+            }
+
+            stringBuilder.append("Case ")
+                    .append(testCases++)
+                    .append(": ")
+                    .append(mapping.getOrDefault(word, "UNKNOWN"))
+                    .append("\n");
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
