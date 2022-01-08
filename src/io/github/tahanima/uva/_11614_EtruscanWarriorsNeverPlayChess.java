fb33ad3ea89f3a1f35bd88daa68dac230b5dab3f
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-08 22:44:14 +0600

    Add UVa 11614 solution

---
+++ src/io/github/tahanima/uva/_11614_EtruscanWarriorsNeverPlayChess.java
@@ -0,0 +1,27 @@
+package io.github.tahanima.uva;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 01/08/2022
+ */
+public class _11614_EtruscanWarriorsNeverPlayChess {
+    public static long findRow(long n) {
+        return (-1 + (long) Math.sqrt(1 + 8 * n)) / 2L;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        int cases = scanner.nextInt();
+
+        for (int i = 1; i <= cases; i++) {
+            long n = scanner.nextLong();
+            stringBuilder.append(findRow(n))
+                    .append("\n");
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
