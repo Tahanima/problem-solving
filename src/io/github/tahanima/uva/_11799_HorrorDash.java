e3cbca6ea553f1cf62c410aaf3599015ee2b0cca
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-09 00:32:18 +0600

    Add UVa 11799 solution

---
+++ src/io/github/tahanima/uva/_11799_HorrorDash.java
@@ -0,0 +1,32 @@
+package io.github.tahanima.uva;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 01/09/2022
+ */
+public class _11799_HorrorDash {
+    public static void main(String[] ags) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        int cases = scanner.nextInt();
+
+        for (int i = 1; i <= cases; i++) {
+            int n = scanner.nextInt();
+            int maxValue = scanner.nextInt();
+
+            while (n-- > 1) {
+                maxValue = Math.max(maxValue, scanner.nextInt());
+            }
+
+            stringBuilder.append("Case ")
+                    .append(i)
+                    .append(": ")
+                    .append(maxValue)
+                    .append("\n");
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
