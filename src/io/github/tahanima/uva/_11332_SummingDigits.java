aeaa08d8d5d99021c2da9c331436de15e2c8b1bf
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-10 23:16:54 +0600

    Add UVa 11332 solution

---
+++ src/io/github/tahanima/uva/_11332_SummingDigits.java
@@ -0,0 +1,41 @@
+package io.github.tahanima.uva;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 01/10/2022
+ */
+public class _11332_SummingDigits {
+    public static int getSum(int n) {
+        if (n < 10) {
+            return n;
+        } else {
+            int sum = 0;
+
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+
+            return getSum(sum);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+
+            if (n == 0)
+                break;
+
+            stringBuilder.append(getSum(n))
+                    .append("\n");
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
