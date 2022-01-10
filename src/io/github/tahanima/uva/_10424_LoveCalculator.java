22d48d54bd59d05403e322c332dcc9990d9ee056
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-10 23:40:18 +0600

    Add UVa 10424 solution

---
+++ src/io/github/tahanima/uva/_10424_LoveCalculator.java
@@ -0,0 +1,51 @@
+package io.github.tahanima.uva;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 01/10/2022
+ */
+public class _10424_LoveCalculator {
+    public static int convert(String name) {
+        int ans = 0;
+
+        for (char ch : name.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                ch = Character.toLowerCase(ch);
+                ans += ch - 'a' + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static int getSum(int n) {
+        if (n < 10) {
+            return n;
+        } else {
+            int sum = 0;
+
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+
+            return getSum(sum);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (scanner.hasNextLine()) {
+            int first = getSum(convert(scanner.nextLine()));
+            int second = getSum(convert(scanner.nextLine()));
+
+            stringBuilder.append(String.format("%.2f %%\n", 100.0 * Math.min(first, second) / Math.max(first, second)));
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
