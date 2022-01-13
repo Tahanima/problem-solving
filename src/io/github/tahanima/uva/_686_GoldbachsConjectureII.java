8c3b971975c36ca1573f0b627ffc1c4e49aa8dae
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-14 01:58:08 +0600

    Add UVa 686 solution

---
+++ src/io/github/tahanima/uva/_686_GoldbachsConjectureII.java
@@ -0,0 +1,79 @@
+package io.github.tahanima.uva;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 01/14/2022
+ */
+public class _686_GoldbachsConjectureII {
+    final static int MAX = 32770;
+    final static ArrayList<Integer> primes = new ArrayList<>();
+
+    public static void generatePrimes() {
+        boolean[] prime = new boolean[MAX];
+        int sqrt = (int) Math.sqrt(MAX);
+
+        for (int i = 3; i <= sqrt; i += 2) {
+            if (!prime[i]) {
+                for (int j = i * i; j < MAX; j += i + i) {
+                    prime[j] = true;
+                }
+            }
+        }
+
+        primes.add(2);
+        for (int i = 3; i < MAX; i += 2) {
+            if (!prime[i]) {
+                primes.add(i);
+            }
+        }
+    }
+
+    public static int count(int n) {
+        int answer = 0;
+        int endIndex = Collections.binarySearch(primes, n);
+
+        if (endIndex < 0) {
+            endIndex = - endIndex - 1;
+        }
+
+        endIndex = Math.min(endIndex, primes.size() - 1);
+
+        for (int i = 0, j = endIndex; i <= j; ) {
+            int a = primes.get(i);
+            int b = primes.get(j);
+
+            if (a + b == n) {
+                answer++;
+                i++; j--;
+            } else if (a + b > n) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        generatePrimes();
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+
+            if (n == 0) {
+                break;
+            }
+
+            stringBuilder.append(String.format("%d\n", count(n)));
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
