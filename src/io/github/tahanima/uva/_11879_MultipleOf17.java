7f6eba79af003d66a53a4933cd3663213a26d111
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-15 02:33:45 +0600

    Add UVa 11879 solution

---
+++ src/io/github/tahanima/uva/_11879_MultipleOf17.java
@@ -0,0 +1,26 @@
+package io.github.tahanima.uva;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 01/15/2022
+ */
+public class _11879_MultipleOf17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (scanner.hasNext()) {
+            BigInteger num = scanner.nextBigInteger();
+
+            if (num.equals(BigInteger.ZERO))
+                break;
+
+            stringBuilder.append(String.format("%d%n", num.mod(BigInteger.valueOf(17)).equals(BigInteger.ZERO) ? 1 : 0));
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
