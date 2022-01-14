0e4468edd15901685bdca6fde7fdf1bf93a10115
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-15 02:18:44 +0600

    Add UVa 424 solution

---
+++ src/io/github/tahanima/uva/_424_IntegerInquiry.java
@@ -0,0 +1,26 @@
+package io.github.tahanima.uva;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 01/15/2022
+ */
+public class _424_IntegerInquiry {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger sum = BigInteger.ZERO;
+
+        while (scanner.hasNext()) {
+            BigInteger num = scanner.nextBigInteger();
+
+            if (num.equals(BigInteger.ZERO))
+                break;
+
+            sum = sum.add(num);
+        }
+
+        System.out.println(sum);
+    }
+}
