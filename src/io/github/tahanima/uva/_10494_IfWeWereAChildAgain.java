2435675cdc9baf9161a48def400e65d5886b08c7
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-21 20:26:10 +0600

    Add UVa 10494 solution

---
+++ src/io/github/tahanima/uva/_10494_IfWeWereAChildAgain.java
@@ -0,0 +1,29 @@
+package io.github.tahanima.uva;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 01/21/2022
+ */
+public class _10494_IfWeWereAChildAgain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (scanner.hasNext()) {
+            String num1 = scanner.next();
+            char operator = scanner.next().charAt(0);
+            int num2 = scanner.nextInt();
+
+            if (operator == '/') {
+               stringBuilder.append(String.format("%s%n", (new BigInteger(num1)).divide(BigInteger.valueOf(num2))));
+            } else {
+                stringBuilder.append(String.format("%s%n", (new BigInteger(num1)).mod(BigInteger.valueOf(num2))));
+            }
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
