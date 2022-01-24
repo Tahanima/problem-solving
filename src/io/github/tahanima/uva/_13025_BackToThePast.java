3629ae00680c85abed5578596e133bf7b8fb2698
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-24 11:31:45 +0600

    Add UVa 13025 solution

---
+++ src/io/github/tahanima/uva/_13025_BackToThePast.java
@@ -0,0 +1,11 @@
+package io.github.tahanima.uva;
+
+/*
+ * @author tahanima
+ * @since 01/24/2022
+ */
+public class _13025_BackToThePast {
+    public static void main(String[] args) {
+        System.out.println("May 29, 2013 Wednesday");
+    }
+}
