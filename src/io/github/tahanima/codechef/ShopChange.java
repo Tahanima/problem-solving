461a2818937923ee7d3adb8fb639822184d426b2
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-07 12:04:20 +0600

    Add CodeChef SHOPCHANGE solution

---
+++ src/io/github/tahanima/codechef/ShopChange.java
@@ -0,0 +1,18 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/07/2022
+ */
+public class ShopChange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            System.out.println(100 - scanner.nextInt());
+        }
+    }
+}
