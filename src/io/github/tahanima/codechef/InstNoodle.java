aa0f34d72b7bcefdcfd407ef147013c545f95d87
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-07 11:41:46 +0600

    Add CodeChef INSTNOODLE solution

---
+++ src/io/github/tahanima/codechef/InstNoodle.java
@@ -0,0 +1,14 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/07/2022
+ */
+public class InstNoodle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() * scanner.nextInt());
+    }
+}
