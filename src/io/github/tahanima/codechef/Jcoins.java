b013bbf33d60f89966faf4b96f0a88c46891294b
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-07 12:13:03 +0600

    Add CodeChef JCOINS solution

---
+++ src/io/github/tahanima/codechef/Jcoins.java
@@ -0,0 +1,18 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/07/2022
+ */
+public class Jcoins {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            System.out.println(10 * scanner.nextInt() + 5 * scanner.nextInt());
+        }
+    }
+}
