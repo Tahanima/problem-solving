bf67c0f5a4c9ad8b083c7848f4c49aed5c138956
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-07 11:54:26 +0600

    Add CodeChef JASSIGNMENTS solution

---
+++ src/io/github/tahanima/codechef/Jassignments.java
@@ -0,0 +1,18 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/07/2022
+ */
+public class Jassignments {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            System.out.printf("%s%n", scanner.nextInt() + 3 <= 10 ? "Yes" : "No");
+        }
+    }
+}
