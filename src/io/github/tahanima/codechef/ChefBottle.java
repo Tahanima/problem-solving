0af52337d780dad444cc703d7389e820d0c97719
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-26 00:29:13 +0600

    Add CodeChef CHEFBOTTLE solution

---
+++ src/io/github/tahanima/codechef/ChefBottle.java
@@ -0,0 +1,22 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/25/2022
+ */
+public class ChefBottle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            int n = scanner.nextInt();
+            int x = scanner.nextInt();
+            int k = scanner.nextInt();
+
+            System.out.println(Math.min(n, k / x));
+        }
+    }
+}
