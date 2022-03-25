4cf24e72d6237d445f3e9cc978999dbd246576c7
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-25 17:21:21 +0600

    Add CodeChef CREDSCORE solution

---
+++ src/io/github/tahanima/codechef/CredScore.java
@@ -0,0 +1,14 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/25/2022
+ */
+public class CredScore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("%s%n", scanner.nextInt() >= 750 ? "YES" : "NO");
+    }
+}
