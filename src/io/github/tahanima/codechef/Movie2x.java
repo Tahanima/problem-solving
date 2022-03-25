4a61a1faea7618898873737110102183df3da10d
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-25 23:59:11 +0600

    Add CodeChef MOVIE2X solution

---
+++ src/io/github/tahanima/codechef/Movie2x.java
@@ -0,0 +1,18 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/25/2022
+ */
+public class Movie2x {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        System.out.println((x - y) + (y / 2));
+    }
+}
