708ee45cc224f3a7e48f8de2ebb786b090371bce
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-25 19:45:44 +0600

    Add CodeChef TYRE solution

---
+++ src/io/github/tahanima/codechef/Tyre.java
@@ -0,0 +1,18 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/25/2022
+ */
+public class Tyre {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            System.out.printf("%s%n", scanner.nextInt() * 2 + scanner.nextInt() * 4);
+        }
+    }
+}
