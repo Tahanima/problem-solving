5c8d93b07b91b1525baed0c628d92556d2a7b86d
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-25 20:05:30 +0600

    Add CodeChef HELIUM3 solution

---
+++ src/io/github/tahanima/codechef/Helium3.java
@@ -0,0 +1,19 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/25/2022
+ */
+public class Helium3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            System.out.printf("%s%n",
+                    (scanner.nextInt() * scanner.nextInt()) <= (scanner.nextInt() * scanner.nextInt()) ? "Yes" : "No");
+        }
+    }
+}
