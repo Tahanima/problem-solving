536c33e1bbd935efcbe6f0bf9a87409f7d326293
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-25 23:30:43 +0600

    Add CodeChef VALENTINE solution

---
+++ src/io/github/tahanima/codechef/Valentine.java
@@ -0,0 +1,18 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/25/2022
+ */
+public class Valentine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            System.out.printf("%d%n", scanner.nextInt() / scanner.nextInt());
+        }
+    }
+}
