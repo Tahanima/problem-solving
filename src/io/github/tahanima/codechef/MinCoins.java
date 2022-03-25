0caeb5ae247fc0e448db1caf11720678f82b3a03
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-25 17:57:25 +0600

    Add CodeChef MINCOINS solution

---
+++ src/io/github/tahanima/codechef/MinCoins.java
@@ -0,0 +1,28 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/25/2022
+ */
+public class MinCoins {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            int amount = scanner.nextInt();
+
+            if (amount % 5 != 0) {
+                System.out.println(-1);
+            } else {
+                int answer = amount / 10;
+                amount -= 10 * answer;
+                answer += amount / 5;
+
+                System.out.printf("%d%n", answer);
+            }
+        }
+    }
+}
