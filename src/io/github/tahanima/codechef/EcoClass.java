3d7228a517de2f0309e1063cd14dc0be5d530865
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-25 18:21:57 +0600

    Add CodeChef ECOCLASS solution

---
+++ src/io/github/tahanima/codechef/EcoClass.java
@@ -0,0 +1,34 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/25/2022
+ */
+public class EcoClass {
+    static final int MAX = 105;
+    static final int[] supply = new int[MAX];
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            int n = scanner.nextInt();
+            int answer = 0;
+
+            for (int i = 0; i < n; i++) {
+                supply[i] = scanner.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (supply[i] == scanner.nextInt()) {
+                    answer++;
+                }
+            }
+
+            System.out.println(answer);
+        }
+    }
+}
