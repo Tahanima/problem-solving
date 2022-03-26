57227587bceb91b3cd7b1d0f2cd33225b54637cb
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-26 15:54:31 +0600

    Add CodeChef BLACKJACK solution

---
+++ src/io/github/tahanima/codechef/Blackjack.java
@@ -0,0 +1,26 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/26/2022
+ */
+public class Blackjack {
+    public static boolean between(int number, int start, int end) {
+        return number >= start && number <= end;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = 21 - a - b;
+
+            System.out.printf("%d%n", between(c, 1, 10) ? c : -1);
+        }
+    }
+}
