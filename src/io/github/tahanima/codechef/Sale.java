d3e54dbb0ee659c4ca47b3f1e0567046befecb10
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-26 20:40:53 +0600

    Add CodeChef SALE solution

---
+++ src/io/github/tahanima/codechef/Sale.java
@@ -0,0 +1,22 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/26/2022
+ */
+public class Sale {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+
+            System.out.println(a + b + c - Math.min(a, Math.min(b, c)));
+        }
+    }
+}
