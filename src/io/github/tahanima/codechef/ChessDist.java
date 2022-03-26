c0e23cd54d8cdd9924beecdb2c8e74fa4519bd60
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-26 16:06:50 +0600

    Add CodeChef CHESSDIST solution

---
+++ src/io/github/tahanima/codechef/ChessDist.java
@@ -0,0 +1,23 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/26/2022
+ */
+public class ChessDist {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            int x1 = scanner.nextInt();
+            int y1 = scanner.nextInt();
+            int x2 = scanner.nextInt();
+            int y2 = scanner.nextInt();
+
+            System.out.println(Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2)));
+        }
+    }
+}
