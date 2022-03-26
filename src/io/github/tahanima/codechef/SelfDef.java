9217f2437c0043bc0fb26210e9e906014d284b92
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-26 16:18:17 +0600

    Add CodeChef SELFDEF solution

---
+++ src/io/github/tahanima/codechef/SelfDef.java
@@ -0,0 +1,30 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/26/2022
+ */
+public class SelfDef {
+    public static boolean between(int number, int start, int end) {
+        return number >= start && number <= end;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            int n = scanner.nextInt();
+            int ans = 0;
+
+            while (n-- > 0) {
+                if (between(scanner.nextInt(), 10, 60))
+                    ans++;
+            }
+
+            System.out.println(ans);
+        }
+    }
+}
