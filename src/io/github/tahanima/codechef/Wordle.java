a434fdeca1fed1ad4ad9376a5e1da23ee98ada6b
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-03-26 20:09:00 +0600

    Add CodeChef WORDLE solution

---
+++ src/io/github/tahanima/codechef/Wordle.java
@@ -0,0 +1,27 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 03/26/2022
+ */
+public class Wordle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            String s = scanner.next();
+            String t = scanner.next();
+            StringBuilder ans = new StringBuilder();
+            int len = s.length();
+
+            for (int i = 0; i < len; i++) {
+                ans.append(String.format("%c", s.charAt(i) == t.charAt(i) ? 'G' : 'B'));
+            }
+
+            System.out.println(ans);
+        }
+    }
+}
