5268244bdc386351297ac65f966032c45bacb669
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-03 23:56:26 +0600

    Add CodeChef BOTTOM solution

---
+++ src/io/github/tahanima/codechef/Bottom.java
@@ -0,0 +1,18 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 04/03/2022
+ */
+public class Bottom {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            System.out.println(7 - scanner.nextInt());
+        }
+    }
+}
