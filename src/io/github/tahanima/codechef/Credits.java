a34e290bfb4c06a21a18556ef2e6d08eb2724e75
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-03 23:40:24 +0600

    Add CodeChef CREDITS solution

---
+++ src/io/github/tahanima/codechef/Credits.java
@@ -0,0 +1,26 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 04/03/2022
+ */
+public class Credits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            int x = scanner.nextInt();
+
+            if (x > 65) {
+                System.out.println("Overload");
+            } else if (x < 35) {
+                System.out.println("Underload");
+            } else {
+                System.out.println("Normal");
+            }
+        }
+    }
+}
