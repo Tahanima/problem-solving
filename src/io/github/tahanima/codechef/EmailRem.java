52851b6e22fbe98e0cb874b6ab7214d1fb771037
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-03 21:19:59 +0600

    Add CodeChef EMAILREM solution

---
+++ src/io/github/tahanima/codechef/EmailRem.java
@@ -0,0 +1,14 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 04/03/2022
+ */
+public class EmailRem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() - scanner.nextInt());
+    }
+}
