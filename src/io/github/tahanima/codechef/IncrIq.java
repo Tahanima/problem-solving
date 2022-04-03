936d311742e77e2d35ee52c4d743ed2a138e8147
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-03 23:11:08 +0600

    Add CodeChef INCRIQ solution

---
+++ src/io/github/tahanima/codechef/IncrIq.java
@@ -0,0 +1,14 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 04/03/2022
+ */
+public class IncrIq {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("%s%n", scanner.nextInt() + 7 > 170 ? "Yes" : "No");
+    }
+}
