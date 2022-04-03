75cd98d4a81bb7591ce2120feeae588c7def558c
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-03 22:14:41 +0600

    Add CodeChef CHEFPAGES solution

---
+++ src/io/github/tahanima/codechef/ChefPages.java
@@ -0,0 +1,23 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 04/03/2022
+ */
+public class ChefPages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a == 1 && b == 1) {
+            System.out.println("https://discuss.codechef.com");
+        } else if (a == 1) {
+            System.out.println("https://www.codechef.com/contests");
+        } else {
+            System.out.println("https://www.codechef.com/practice");
+        }
+    }
+}
