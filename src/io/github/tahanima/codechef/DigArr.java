239481c75ebebdb35d19d8614e5eab6b45ed1fbe
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-03 22:27:16 +0600

    Add CodeChef DIGARR solution

---
+++ src/io/github/tahanima/codechef/DigArr.java
@@ -0,0 +1,26 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 04/03/2022
+ */
+public class DigArr {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            boolean canBeMultipleOfFive = false;
+            scanner.nextInt();
+            String num = scanner.next();
+
+            for (char ch : num.toCharArray()) {
+                canBeMultipleOfFive |= (ch == '0') || (ch == '5');
+            }
+
+            System.out.printf("%s%n", canBeMultipleOfFive ? "Yes" : "No");
+        }
+    }
+}
