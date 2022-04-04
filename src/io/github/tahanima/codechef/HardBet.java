24021014f23334018ca47afc483f29b9fe08b0cf
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-04 23:17:54 +0600

    Add CodeChef HARDBET solution

---
+++ src/io/github/tahanima/codechef/HardBet.java
@@ -0,0 +1,23 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 04/04/2022
+ */
+public class HardBet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+            int min = Math.min(a, Math.min(b, c));
+
+            System.out.printf("%s%n", (c == min) ? "Alice" : ((b == min) ? "Bob" : "Draw"));
+        }
+    }
+}
