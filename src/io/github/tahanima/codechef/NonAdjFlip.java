69d55a29749b653bb3e1ea9d00c667ceded2b2f0
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-11 20:05:14 +0600

    Add CodeChef NONADJFLIP solution

---
+++ src/io/github/tahanima/codechef/NonAdjFlip.java
@@ -0,0 +1,40 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/*
+ * @author tahanima
+ * @since 04/11/2022
+ */
+public class NonAdjFlip {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cases = scanner.nextInt();
+
+        while (cases-- > 0) {
+            int n = scanner.nextInt();
+            String binaryStr = scanner.next();
+            boolean hasOne = binaryStr.charAt(0) == '1';
+            boolean hasAdjOnes = false;
+
+            for (int i = 1; i < n; i++) {
+                if (binaryStr.charAt(i) == '1') {
+                    hasOne = true;
+
+                    if (binaryStr.charAt(i - 1) == '1')
+                        hasAdjOnes = true;
+                }
+            }
+
+            if (hasOne) {
+                if (hasAdjOnes) {
+                    System.out.println(2);
+                } else {
+                    System.out.println(1);
+                }
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+}
