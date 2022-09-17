362b51195f20848f7e6498f1d0d8161c47510dea
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-09-17 16:12:17 +0600

    removed additional max calc

--- src/io/github/tahanima/leetcode/_121_BestTimeToBuyAndSellStock.java
+++ src/io/github/tahanima/leetcode/_121_BestTimeToBuyAndSellStock.java
@@ -6,16 +6,12 @@ package io.github.tahanima.leetcode;
 public class _121_BestTimeToBuyAndSellStock {
     public int maxProfit(int[] prices) {
         int size = prices.length;
-        int [] maxNumFromIndex = new int[size];
-        int i = size;
-
-        while (--i >= 0) {
-            maxNumFromIndex[i] = (i == (size - 1)) ? prices[i] : Math.max(prices[i], maxNumFromIndex[i + 1]);
-        }
-
+        int maxNumFromIndex = 0;
         int max = 0;
-        for (i = 0; i < size; i++) {
-            max = Math.max(max, maxNumFromIndex[i] - prices[i]);
+
+        for (int i = size - 1; i >= 0; i--) {
+            maxNumFromIndex = (i == size - 1) ? prices[i] : Math.max(maxNumFromIndex, prices[i]);
+            max = Math.max(max, maxNumFromIndex - prices[i]);
         }
 
         return Math.max(0, max);
