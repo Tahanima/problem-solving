5981cb1a9d7e12d6eb4907a08a44cb112ed56060
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-11-18 21:26:01 +0600

    update

--- src/io/github/tahanima/leetcode/_15_3Sum.java
+++ src/io/github/tahanima/leetcode/_15_3Sum.java
@@ -6,44 +6,23 @@ import java.util.*;
  * @author tahanima
  */
 public class _15_3Sum {
-    public void diff(ArrayList<Integer> lis0, ArrayList<Integer> lis1, HashSet<ArrayList<Integer>> hs) {
-        int n = lis0.size();
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashSet<ArrayList<Integer>> hs = new HashSet<>();
+        Arrays.sort(nums);
+        int n = nums.length;
 
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                int num = -(lis0.get(i) + lis0.get(j));
-
-                if (Collections.binarySearch(lis1, num) >= 0) {
-                    ArrayList<Integer> triplet = new ArrayList<>();
-                    triplet.add(lis0.get(i));
-                    triplet.add(lis0.get(j));
-                    triplet.add(num);
-
-                    Collections.sort(triplet);
-                    hs.add(triplet);
-                }
-            }
-        }
-    }
-
-    public void self(ArrayList<Integer> lis0, HashSet<ArrayList<Integer>> hs) {
-        int n = lis0.size();
-        int[] temp = new int[n];
-
-        for (int i = 0; i < n; temp[i] = lis0.get(i), i++);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int num = -(lis0.get(i) + lis0.get(j));
+                int num = -(nums[i] + nums[j]);
 
                 if (j == (n - 1)) {
                     continue;
                 }
 
-                if (Arrays.binarySearch(temp, j + 1, n, num) >= 0) {
+                if (Arrays.binarySearch(nums, j + 1, n, num) >= 0) {
                     ArrayList<Integer> triplet = new ArrayList<>();
-                    triplet.add(lis0.get(i));
-                    triplet.add(lis0.get(j));
+                    triplet.add(nums[i]);
+                    triplet.add(nums[j]);
                     triplet.add(num);
 
                     Collections.sort(triplet);
@@ -51,29 +30,6 @@ public class _15_3Sum {
                 }
             }
         }
-    }
-
-    public List<List<Integer>> threeSum(int[] nums) {
-        ArrayList<Integer> lis0 = new ArrayList<>();
-        ArrayList<Integer> lis1 = new ArrayList<>();
-        HashSet<ArrayList<Integer>> hs = new HashSet<>();
-        int n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-            if ((i & 1) == 0) {
-                lis0.add(nums[i]);
-            } else {
-                lis1.add(nums[i]);
-            }
-        }
-
-        Collections.sort(lis0);
-        Collections.sort(lis1);
-
-        diff(lis0, lis1, hs);
-        diff(lis1, lis0, hs);
-        self(lis0, hs);
-        self(lis1, hs);
 
         return new ArrayList<>(hs);
     }
