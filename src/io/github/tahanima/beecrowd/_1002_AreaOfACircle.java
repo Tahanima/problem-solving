5dff6575385b0c878e1dc6c2218b02218e413fa5
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-11-19 23:04:22 +0600

    Added beecrowd 1002 solution

---
+++ src/io/github/tahanima/beecrowd/_1002_AreaOfACircle.java
@@ -0,0 +1,13 @@
+package io.github.tahanima.beecrowd;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class _1002_AreaOfACircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("A=%.4f%n", 3.14159 * Math.pow(sc.nextDouble(), 2));
+    }
+}
