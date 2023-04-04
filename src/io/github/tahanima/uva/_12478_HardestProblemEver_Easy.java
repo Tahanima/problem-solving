e01b5d729a9f3a707954f42b32ec4d0e9eba6fff
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2023-04-04 22:47:06 +0600

    Add UVa 12478 solution

---
+++ src/io/github/tahanima/uva/_12478_HardestProblemEver_Easy.java
@@ -0,0 +1,10 @@
+package io.github.tahanima.uva;
+
+/**
+ * @author tahanima
+ */
+public class _12478_HardestProblemEver_Easy {
+    public static void main(String[] args) {
+        System.out.println("KABIR");
+    }
+}
