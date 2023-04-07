2aca27c7f436446d0b9f2dc56e66138b6e2946cd
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2023-04-07 15:00:39 +0600

    Added CodeChef START01 solution

---
+++ src/io/github/tahanima/codechef/Start01.java
@@ -0,0 +1,13 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Start01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt());
+    }
+}
