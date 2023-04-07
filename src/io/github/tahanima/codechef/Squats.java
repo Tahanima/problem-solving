372c010658d8cdd0c2e26e94a922ef2f88f5e974
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2023-04-07 15:00:28 +0600

    Added CodeChef SQUATS solution

---
+++ src/io/github/tahanima/codechef/Squats.java
@@ -0,0 +1,18 @@
+package io.github.tahanima.codechef;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Squats {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+
+        while (T-- > 0) {
+            int X = scanner.nextInt();
+            System.out.println(15 * X);
+        }
+    }
+}
